7a5462ff413e5bff0469f9de81b9ad044b0aee5c
daibao87 <161785741+daibao87@users.noreply.github.com>
2024-12-27 23:08:13 +0800

    Update mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,22 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+@R2
+M=0
+@R0
+D=M
+@STEP
+D;JGT
+@END
+D;JMP
+(STEP)
+D=M
+@R1
+D=D+M
+D=M-1
+M=D
+@STEP
+D;JGT
+(END)
+@END
+D;JMP
